08a8c6445738dd8f5952e74ccbb4a37fdb2d71e3
Jon Stutters <j.stutters@ucl.ac.uk>
2025-08-06 17:01:36 +0100

    Add oil plugin
    Install the oil plugin for filesystem editing.

--- init.lua
+++ init.lua
@@ -284,6 +284,14 @@ require('lazy').setup({
     },
   },
 
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    lazy = false,
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
